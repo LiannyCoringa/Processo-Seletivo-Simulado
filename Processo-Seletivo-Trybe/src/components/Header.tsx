df62c2f3d13b4515b22d9b9fd7c75cb4c9eeae4f
Lianny Coringa <lianny_costa@hotmail.com>
2023-09-17 13:13:23 -0300

    Criando Header

---
+++ Processo-Seletivo-Trybe/src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+      <header>
+        <img src="src/assets/image 68.png" alt="logomarca da Trybe" />
+        <h1>TRYBE NEWS</h1>
+      </header>
+    )
+  }
+
+  export default Header;
